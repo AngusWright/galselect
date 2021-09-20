a9d83374c5132a02678e2acbebd775280e1644c1
jlvdb <jlvdb@astro.uni-bonn.de>
2021-09-20 13:04:22 +0200

    fixed data loading, fixed argument parser names, added --progress, mask data outside redshift range

--- select_gals.py
+++ select_gals.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python3
 import argparse
 
+import numpy as np
 import pandas as pd
 
 import galselect
+
+
+def load_input(fpath):
+    if fpath.endswith(".csv"):
+        data = pd.read_csv(fpath)
+    if fpath.endswith(".fits"):
+        data = galselect.read_fits(fpath)
+    else:
+        raise ValueError(f"fileformat not supported: {fpath}")
+    return data
+
 
 parser = argparse.ArgumentParser()
 
@@ -16,7 +28,7 @@ data.add_argument(
     "-m", "--mock", metavar="path", required=True,
     help="input FITS catalogue which is matched to the data")
 data.add_argument(
-    "-o", "--out", metavar="path", required=True,
+    "-o", "--output", metavar="path", required=True,
     help="matched FITS output mock data catalogue")
 
 _map_kwargs = dict(nargs=2, metavar=("data", "mock"))
@@ -34,51 +46,55 @@ features.add_argument(
 data = parser.add_argument_group(
     "Configuration", description="Optional parameters")
 data.add_argument(
-    "--z-warn", metavar="float", default=0.05,
+    "--z-warn", metavar="float", type=float, default=0.05,
     help="issue a warning if the redshift difference of a match exceeds this "
          "threshold (default: %(default)s")
 data.add_argument(
-    "--idx-interval", metavar="int", default=10000,
+    "--idx-interval", metavar="int", type=int, default=10000,
     help="number of nearest neighbours in redshift used to find a match "
          "(default: %(default)s")
 data.add_argument(
     "--distances", action="store_true",
     help="store the distance in feature space of the matches in the output "
          "catalogue")
+data.add_argument(
+    "--progress", action="store_true",
+    help="display a progress bar")
 
 
 if __name__ == "__main__":
     args = parser.parse_args()
 
     # read the mock and data input catalogues
-    if args.datapath.endswith(".csv"):
-        data = pd.read_csv(args.datapath)
-    if args.datapath.endswith(".fits"):
-        data = galselect.read_fits(args.mockpath)
-    else:
-        raise ValueError(f"fileformat not supported: {args.mockpath}")
-    if args.mockpath.endswith(".csv"):
-        mock = pd.read_csv(args.mockpath)
-    if args.mockpath.endswith(".fits"):
-        mock = galselect.read_fits(args.mockpath)
-    else:
-        raise ValueError(f"fileformat not supported: {args.mockpath}")
+    data = load_input(args.data)
+    mock = load_input(args.mock)
 
     # unpack the redshift column name parameter
     z_name_data, z_name_mock = args.z_name
 
     # unpack the mapping for feature/column names from mock to data
     feature_names_data, feature_names_mock = [], []
-    for feature_data, feature_mock in args.features:
+    for feature_data, feature_mock in args.feature:
         feature_names_data.append(feature_data)
         feature_names_mock.append(feature_mock)
 
     # match the catalogues
     selector = galselect.DataMatcher(
-        data, z_name_data, [f for f in feature_names_data], args.z_warn)
+        mock, z_name_mock, [f for f in feature_names_mock],
+        redshift_warning=args.z_warn)
+    # mask to redshift range
+    mask = (
+        (data[z_name_data] > selector.z_min) &
+        (data[z_name_data] < selector.z_max))
+    nbad = len(mask) - np.count_nonzero(mask)
+    data = data[mask]
+    if nbad != 0:
+        print(
+            f"WARNING: removed {nbad}/{len(mask)} data objects outside the mock"
+            f" redshift range of {selector.z_min:.3f} to {selector.z_max:.3f}")
     matched = selector.match_catalog(
-        mock[args.z_name_mock], mock[[f for f in feature_names_mock]],
-        args.idx_interval, args.distances)
+        data[z_name_data], data[[f for f in feature_names_data]].to_numpy(),
+        args.idx_interval, args.distances, progress=args.progress)
 
     # write 
-    galselect.write_fits(matched, args.matchpath)
+    galselect.write_fits(matched, args.output)
